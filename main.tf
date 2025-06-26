904ae375d74be77b38086a9f0430cd35d69a5712
Parth Sadiwala <sadiwala1364@gmail.com>
2025-06-26 19:03:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
-resource "google_cloud_run_service_iam_member" "authorize" {
-  count    = length(var.members)
-  location = google_cloud_run_service.main.location
-  project  = google_cloud_run_service.main.project
-  service  = google_cloud_run_service.main.name
-  role     = "roles/run.invoker"
-  member   = var.members[count.index]
-}
-
-module "m1" {
-  source = "github.com/GoogleCloudPlatform/terraform-google-regional-lb-http//modules/backend"
+resource "google_storage_bucket" "default" {
+  name          = "gjhwf-bucket"
+  location      = "US"
+  storage_class = "STANDARD"
 }
